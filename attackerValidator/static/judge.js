566f5688c5d9fc13d2c1435c4b92e0b998463d2d
Sythnog <sythnog@protonmail.com>
2021-09-17 14:34:33 +0200

    JavaScript for judge now using correct port

--- attackerValidator/static/judge.js
+++ attackerValidator/static/judge.js
@@ -13,22 +13,22 @@ function StartJudge() {
             const respJson = JSON.parse(httpReq.response);
             if (respJson["failed"] == true) {
                 // As we failed, then redirect user to the failed page
-                window.location = `http://${window.location.hostname}:8080/failed`;
+                window.location = `http://${window.location.hostname}/failed`;
             } else {
                 // Send the user to the done page, where they will get the result from the validation/attack
-                window.location = `http://${window.location.hostname}:8080/done`;
+                window.location = `http://${window.location.hostname}/done`;
             }
             console.log(options)
         }
     }
 
     // Set the URL to /start and send request
-    httpReq.open("post", `http://${window.location.hostname}:8080/start`, true);
+    httpReq.open("post", `http://${window.location.hostname}/start`, true);
     httpReq.send(); 
 }
 
 // Function used to go back to the frontpage
 function Homepage() {
     // Change the browser location to the current domainname
-    window.location = `http://${window.location.hostname}:8080/`;
+    window.location = `http://${window.location.hostname}/`;
 }
