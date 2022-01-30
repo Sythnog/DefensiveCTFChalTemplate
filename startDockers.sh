7768372fd9af417164e47cc6aa42ea300874dd6b
Sythnog <sythnog@protonmail.com>
2022-01-30 18:54:56 +0100

    PEP 8 and Pylint compliant vulnMachine

--- startDockers.sh
+++ startDockers.sh
@@ -8,7 +8,7 @@ echo -e "\n-=== Running new containers ===-\n"
 # Attacker/Validator machine
 attvalID=$(docker run -d --name defensive_ctf_attval_running defensive_ctf_attval_build)
 attvalIP=$(docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' defensive_ctf_attval_running)
-echo -e "Vuln machine can be found on http://$attvalIP\nDocker ID is $attvalID\n"
+echo -e "Attacker/validator machine can be found on http://$attvalIP\nDocker ID is $attvalID\n"
 
 # Vuln machine
 vulnID=$(docker run -d --name defensive_ctf_vuln_running defensive_ctf_vuln_build)
