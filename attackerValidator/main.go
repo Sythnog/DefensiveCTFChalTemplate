2eda6d1af568a54676536cca6e1c3273a8dec205
Sythnog <sythnog@protonmail.com>
2022-01-31 14:52:37 +0100

    New front-end for judge

--- attackerValidator/main.go
+++ attackerValidator/main.go
@@ -1,23 +1,86 @@
 package main
 
+// -=== Setup and init ===-
+
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 )
 
-func main() {
-	// Initialize the Gin framework
-	r := gin.Default()
+// Allow for us to easily group challenge data
+type Challenge struct {
+	challName              string
+	challSolved            bool
+	challWebLocation       string
+	challFlag              string
+	challSSHPass           string
+	challFileLoc           string
+	challRestartServiceCMD string
+}
 
-	// Tmp example endpoint
-	r.GET("/tmp", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"msg": "tmp hello!",
+// -=== Main functionality ===-
+
+// The main function, which outlines and constructs the service
+func main() {
+	// Initialize the Gin framework, and HTML templates
+	r := gin.Default()
+	r.LoadHTMLGlob("webTemplates/*")
+
+	// Store the challenge data in a struct
+	// NOTE: Since they are hard-coded, no input validation will be written
+	challInfo := Challenge {
+		challName:              "SSTI Example Challenge",
+		challSolved:            false,
+		challWebLocation:       "172.17.0.3",
+		challFlag:              "flag{ex@mp1e_FlaG_123}",
+		challSSHPass:           "notS0S3cr3tSSHPa55",
+		challFileLoc:           "/opt/notFoundService",
+		challRestartServiceCMD: "killall python3; python3 /opt/notFoundService/vuln_service.py",
+	}
+
+	// -== Endpoints =-
+
+	// The main page of the judge
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(200, "index.html", gin.H{
+			"challName": challInfo.challName,
 		})
 	})
 
-	// Run the API, and set the port
+	// The FAQ page about the concept and challegne
+	r.GET("/faq", func(c *gin.Context) {
+		c.HTML(200, "faq.html", gin.H{
+			"challName":              challInfo.challName,
+			"challWebLocation":       challInfo.challWebLocation,
+			"challSSHPass":           challInfo.challSSHPass,
+			"challFileLoc":           challInfo.challFileLoc,
+			"challRestartServiceCMD": challInfo.challRestartServiceCMD,
+		})
+	})
+
+	// The page which will diplay the result, once an assessment has been made
+	r.GET("/result", func(c *gin.Context) {
+		// If the challenge has been solved, then return the flag
+		if challInfo.challSolved {
+			c.HTML(200, "resultSolved.html", gin.H{
+				"challName":      challInfo.challName,
+				"challStatusMsg": "You solved the challenge!",
+				"challFlag":      challInfo.challFlag,
+			})
+			return
+		}
+
+		// Upon an invalid result, default to tell this to the user
+		c.HTML(200, "resultBad.html", gin.H{
+			"challName":      challInfo.challName,
+			"challStatusMsg": "The challenge has not been solved :(",
+		})
+	})
+
+	// -== Run the service ==-
+
+	// Run the service, and set the port
 	if err := r.Run(":80"); err != nil {
-		fmt.Print("Unable to start the API :(")
+		fmt.Print("Unable to start the service :(")
 	}
 }
--- attackerValidator/validateCode.sh
+++ attackerValidator/validateCode.sh
@@ -7,16 +7,23 @@ do
     echo "[$goFile]"
     go vet $goFile
     go fmt $goFile
-    ~/.local/bin/semgrep --config "p/golang"
 done
 
 # Check the Dockerfile
 echo "[Dockerfile]"
 ~/.local/bin/semgrep --config "p/dockerfile"
 
-# Use gosec for another SAST security check
+# Perform SAST security checks
 echo -e "\n{Gosec}\n"
 ~/go/bin/gosec .
+echo -e "\n{Semgrep: Golang}\n"
+~/.local/bin/semgrep --config "p/golang"
+echo -e "\n{Semgrep: CI}\n"
+~/.local/bin/semgrep --config "p/ci"
+echo -e "\n{Semgrep: OWASP Top 10}\n"
+~/.local/bin/semgrep --config "p/owasp-top-ten"
+echo -e "\n{Semgrep: R2C Security Audit}\n"
+~/.local/bin/semgrep --config "p/r2c-security-audit"
 
 # Finally run golangcli-lint on all of the code
 echo -e "\n{Golangcli-lint}\n"
